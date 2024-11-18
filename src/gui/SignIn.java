e044d57ec87545a78c438ee46537fc852ac49239
BaranaNadil <161505829+BaranaNadil@users.noreply.github.com>
2024-11-18 18:09:21 +0530

    FrantDeskOver view eka Code Karanava

--- src/gui/SignIn.java
+++ src/gui/SignIn.java
@@ -138,6 +138,7 @@ public class SignIn extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jToggleButton1 = new javax.swing.JToggleButton();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -147,6 +148,8 @@ public class SignIn extends javax.swing.JDialog {
         loginButton = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         logo = new javax.swing.JLabel();
+
+        jToggleButton1.setText("jToggleButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -301,44 +304,6 @@ public class SignIn extends javax.swing.JDialog {
 
     }//GEN-LAST:event_loginButtonActionPerformed
 
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                SignIn dialog = new SignIn(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField emailFild;
@@ -347,6 +312,7 @@ public class SignIn extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JButton loginButton;
     private javax.swing.JLabel logo;
     private javax.swing.JPasswordField passwordFild;
---
+++ src/model/Rooms.java
@@ -0,0 +1,70 @@
+package model;
+
+
+public class Rooms {
+    
+    private int roomId;
+    private String roomNo;
+    private int roomPersonCount;
+    private Double price;
+
+    /**
+     * @return the roomId
+     */
+    public int getRoomId() {
+        return roomId;
+    }
+
+    /**
+     * @param roomId the roomId to set
+     */
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    /**
+     * @return the roomNo
+     */
+    public String getRoomNo() {
+        return roomNo;
+    }
+
+    /**
+     * @param roomNo the roomNo to set
+     */
+    public void setRoomNo(String roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    /**
+     * @return the roomPersonCount
+     */
+    public int getRoomPersonCount() {
+        return roomPersonCount;
+    }
+
+    /**
+     * @param roomPersonCount the roomPersonCount to set
+     */
+    public void setRoomPersonCount(int roomPersonCount) {
+        this.roomPersonCount = roomPersonCount;
+    }
+
+ 
+
+    /**
+     * @return the price
+     */
+    public Double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+    
+}
